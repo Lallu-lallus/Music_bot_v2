b4f60157a7afafea3e360a5698b337be37361634
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-15 16:17:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,17 @@ load_dotenv(dotenv_path)
 TOKEN = os.environ.get("TOKEN")
 bot = telepotpro.Bot(TOKEN)
 
+PHOTO = [
+    "https://telegra.ph/file/8f65af0419d2e4b92bf2a.jpg",
+    "https://telegra.ph/file/099fdd8ae27a68e73c4b4.jpg",
+    "https://telegra.ph/file/3e5d288f42ea557ec5325.jpg",
+    "https://telegra.ph/file/3c77153a47314ac7a99f2.jpg",
+    "https://telegra.ph/file/1050fc48d6402bfdcd7af.jpg",
+    "https://telegra.ph/file/f45a5c0e064411027333c.jpg",
+    "https://telegra.ph/file/8da62823c9508fe054d76.jpg",
+    "https://telegra.ph/file/6c97af926e1f1cd54c146.jpg",
+]
+
 class Music:
     def __init__(self, user_input, msg):
         self.chat = Chat
